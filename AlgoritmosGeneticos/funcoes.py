7cb1b96accc550510362e5953a8a83b8b2df3592
monyque <106678040+monocas@users.noreply.github.com>
2023-03-16 14:43:44 -0300

    Add files via upload

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,28 @@
- 
+from random import choice
+
+def gene_cb():
+    """Gera um gene valido para um indivíduo
+    Return: 
+        Um valor entre 0 ou 1."""
+    genes = [0,1]
+    gene = choice(genes)
+    return(gene)
+    
+def indivíduo_cb(n):
+    """Gera um indivíduo para o problema das caixas binarias.
+    
+    Args:
+        n: número de genes do indivíduo.
+    Return:
+        Uma lista com n genes. Cada gene é um valor zero ou um.
+        """
+    indivíduo = [gene_cb() for i in range(n)]
+    return indivíduo
+
+def função_objetivo_cb(indivíduo):
+    """Computa a função objetivo para um indivíduo
+    Args:
+        Indivíduo: lista contendo todos os genes das caixas binarias.
+    Return:
+        Um valor representando a soma dos genes do indivíduo."""
+    return sum(indivíduo)
